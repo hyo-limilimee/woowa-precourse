bed8f18721e3cff7c2cb98f70822a6123821a313
hyo-limilimee <1001lily@naver.com>
2023-11-11 03:53:59 +0900

    feat: add menu input logic

---
+++ src/main/java/christmas/Model/Menu.java
@@ -0,0 +1,27 @@
+package christmas.Model;
+
+public enum Menu {
+    PINE_MUSHROOM_SOUP("양송이수프", 6_000, "appetizer"),
+    TAPAS("타파스", 5_500, "appetizer"),
+    CAESAR_SALAD("시저샐러드", 8_000, "appetizer"),
+    T_BORN_STEAK("티본스테이크", 5_5000, "main"),
+    BARBECUE_RIBS("바비큐립", 5_4000, "main"),
+    SEAFOOD_PASTA("해산물파스타", 35_000, "main"),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, "main"),
+    CHOCOLATE_CAKE("초코케이크", 15_000, "dessert"),
+    ICECREAM("아이스크림", 5_000, "dessert"),
+    ZERO_COLA("제로콜라", 3_000, "drink"),
+    RED_WINE("레드와인", 60_000, "drink"),
+    CHAMPANE("샴페인", 25_000, "drink");
+
+    private String menuName;
+    private int menuPrice;
+    private String menuType;
+    Menu(String menuName, int menuPrice, String menuType)
+    {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuType = menuType;
+    }
+
+}

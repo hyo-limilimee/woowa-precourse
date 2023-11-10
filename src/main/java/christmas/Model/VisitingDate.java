6f781d0da4fd63b6c11bfe1d131a8f64d4d61cde
hyo-limilimee <1001lily@naver.com>
2023-11-11 02:44:28 +0900

    feat: add date input and validate functions

---
+++ src/main/java/christmas/Model/VisitingDate.java
@@ -0,0 +1,31 @@
+package christmas.Model;
+
+import christmas.View.ErrorMessages;
+
+public class VisitingDate {
+    private final int date;
+    private final static int MIN_DATE_NUMBER = 1;
+    private final static int MAX_DATE_NUMBER = 31;
+
+    public VisitingDate(String dateInput) {
+        int dateInputNum = validateIsNumeric(dateInput);
+        validateNumberInRange(dateInputNum);
+        this.date = dateInputNum;
+    }
+
+    private static int validateIsNumeric(String dateInput) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(dateInput);
+        } catch (NumberFormatException e) {
+            ErrorMessages.dateNumberTypeError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateNumberInRange(int date) {
+        if (date < MIN_DATE_NUMBER || date > MAX_DATE_NUMBER) {
+            ErrorMessages.dateNumberRangeError();
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/main/java/christmas/View/InputView.java
@@ -0,0 +1,16 @@
+package christmas.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
+    public String readDate() {
+        System.out.println(DATE_INPUT_MESSAGE);
+        String dateInput = Console.readLine();
+
+        return dateInput;
+    }
+
+
+}

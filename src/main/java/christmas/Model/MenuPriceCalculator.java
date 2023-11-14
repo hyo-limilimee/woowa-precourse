c6500e5d29368a75943c18a557470a7fa3e2797c
hyo-limilimee <1001lily@naver.com>
2023-11-14 20:27:38 +0900

    refactor: reduce depths of MenuPriceCalcultor class

--- src/main/java/christmas/Model/MenuPriceCalculator.java
+++ src/main/java/christmas/Model/MenuPriceCalculator.java
@@ -3,25 +3,37 @@ package christmas.Model;
 import java.util.List;
 
 public class MenuPriceCalculator {
+    private static final int PRESENTATION_STANDARD_AMOUNT = 120000;
     public static int calculateTotalPrice(List<OrderedList> orderList) {
         int totalPrice = 0;
 
         for (OrderedList orderedItem : orderList) {
-            String menuName = orderedItem.menuName;
-            int quantity = orderedItem.menuQuantity;
+            totalPrice = addTotalPriceForItem(orderedItem, totalPrice);
+        }
 
-            for (Menu menu : Menu.values()) {
-                if (menu.getMenuName().equals(menuName)) {
-                    totalPrice += menu.getMenuPrice() * quantity;
-                    break;
-                }
-            }
+        return totalPrice;
+    }
+
+    private static int addTotalPriceForItem(OrderedList orderedItem, int totalPrice) {
+        String menuName = orderedItem.menuName;
+        int quantity = orderedItem.menuQuantity;
+
+        for (Menu menu : Menu.values()) {
+            totalPrice = addEachPrice(menu, menuName, quantity, totalPrice);
+        }
+
+        return totalPrice;
+    }
+
+    private static int addEachPrice(Menu menu, String menuName, int quantity, int totalPrice) {
+        if (menu.getMenuName().equals(menuName)) {
+            return totalPrice + menu.getMenuPrice() * quantity;
         }
         return totalPrice;
     }
 
     public static boolean isPresent(int totalPrice) {
-        if (totalPrice > 120000) {
+        if (totalPrice > PRESENTATION_STANDARD_AMOUNT) {
             return true;
         }
         return false;

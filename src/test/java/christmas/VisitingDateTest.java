35851fe404de8ce5617f9a4ef34a43295b750334
hyo-limilimee <1001lily@naver.com>
2023-11-15 19:17:27 +0900

    test: modify testcodes

--- src/test/java/christmas/VisitingDateTest.java
+++ src/test/java/christmas/VisitingDateTest.java
@@ -1,30 +1,30 @@
-//package christmas;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-//
-//import christmas.Model.VisitingDate;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//public class VisitingDateTest {
-//
-//    @DisplayName("방문 날짜 입력이 숫자가 아닌 경우에 대한 예외 처리")
-//    @Test
-//    void NonNumericInput() {
-//        String invalidDateInput = "abc";
-//
-//        assertThatThrownBy(() ->
-//                new VisitingDate(invalidDateInput))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @DisplayName("방문 날짜 입력이 숫자가 아닌 경우에 대한 예외 처리")
-//    @Test
-//    void NumberOutRangeInput() {
-//        String invalidDateInput = "32";
-//
-//        assertThatThrownBy(() ->
-//                new VisitingDate(invalidDateInput))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//}
+package christmas;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import christmas.Model.VisitingDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class VisitingDateTest {
+
+    @DisplayName("방문 날짜 입력이 숫자가 아닌 경우에 대한 예외 처리")
+    @Test
+    void NonNumericInput() {
+        String invalidDateInput = "abc";
+
+        assertThatThrownBy(() ->
+                new VisitingDate(invalidDateInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("방문 날짜 입력이 숫자가 아닌 경우에 대한 예외 처리")
+    @Test
+    void NumberOutRangeInput() {
+        String invalidDateInput = "32";
+
+        assertThatThrownBy(() ->
+                new VisitingDate(invalidDateInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

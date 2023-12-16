c2a4a67a31c103a8650dc1c264b081c0353162f4
hyo-limilimee <1001lily@naver.com>
2023-12-16 18:10:11 +0900

    refactor: make method shorter

--- src/main/java/oncall/Model/WeekDayTurn.java
+++ src/main/java/oncall/Model/WeekDayTurn.java
@@ -8,42 +8,49 @@ import oncall.View.ErrorMessages;
 
 public class WeekDayTurn {
 
-    private static final Integer MAX_COUTNT = 35;
-    private static final Integer MIN_COUTNT = 5;
-    private static final Integer MAX_NICKNAME_COUTNT = 5;
+    private static final Integer MAX_COUNT = 35;
+    private static final Integer MIN_COUNT = 5;
+    private static final Integer MAX_NICKNAME_COUNT = 5;
     private static final String COMMA = ",";
-
 
     public static List<String> parseTurnInput(String weekdayTurnInput) {
         List<String> weekdayTurnList = new ArrayList<>();
-        Set<String> uniqueNicknames = new HashSet<>();  // To check for duplicates
+        Set<String> uniqueNicknames = new HashSet<>();
         String[] weekdayTurns = weekdayTurnInput.split(COMMA);
 
-        if (weekdayTurns.length < MIN_COUTNT || weekdayTurns.length > MAX_COUTNT) {
-            ErrorMessages.nicknameCountError();
-            throw new IllegalArgumentException();
-        }
+        validateTurnCount(weekdayTurns.length);
 
         for (String weekdayTurn : weekdayTurns) {
             String trimmedTurn = weekdayTurn.trim();
-            if (!trimmedTurn.isEmpty() && validateNickName(trimmedTurn)) {
-                if (uniqueNicknames.contains(trimmedTurn)) {
-                    ErrorMessages.nicknameDuplicateError();
-                    throw new IllegalArgumentException();
-                } else {
-                    weekdayTurnList.add(trimmedTurn);
-                    uniqueNicknames.add(trimmedTurn);
-                }
-            } else {
-                ErrorMessages.nicknameLengthError();
-                throw new IllegalArgumentException();
-            }
+            validateAndAddNickname(trimmedTurn, uniqueNicknames, weekdayTurnList);
         }
 
         return weekdayTurnList;
     }
 
+    private static void validateTurnCount(int turnCount) {
+        if (turnCount < MIN_COUNT || turnCount > MAX_COUNT) {
+            ErrorMessages.nicknameCountError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateAndAddNickname(String nickname, Set<String> uniqueNicknames, List<String> weekdayTurnList) {
+        if (!nickname.isEmpty() && validateNickName(nickname)) {
+            if (uniqueNicknames.contains(nickname)) {
+                ErrorMessages.nicknameDuplicateError();
+                throw new IllegalArgumentException();
+            } else {
+                weekdayTurnList.add(nickname);
+                uniqueNicknames.add(nickname);
+            }
+        } else {
+            ErrorMessages.nicknameLengthError();
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static boolean validateNickName(String nickname) {
-        return nickname.length() <= MAX_NICKNAME_COUTNT;
+        return nickname.length() <= MAX_NICKNAME_COUNT;
     }
 }

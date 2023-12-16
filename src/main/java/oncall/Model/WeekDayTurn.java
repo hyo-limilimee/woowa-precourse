4b902db6db63ff7eb48f71c9793500fb8fc12b94
hyo-limilimee <1001lily@naver.com>
2023-12-16 15:01:13 +0900

    feat: make error about duplicated member in list

--- src/main/java/oncall/Model/WeekDayTurn.java
+++ src/main/java/oncall/Model/WeekDayTurn.java
@@ -1,19 +1,28 @@
 package oncall.Model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import oncall.View.ErrorMessages;
 
 public class WeekDayTurn {
 
     public static List<String> parseTurnInput(String weekdayTurnInput) {
         List<String> weekdayTurnList = new ArrayList<>();
+        Set<String> uniqueNicknames = new HashSet<>();  // To check for duplicates
         String[] weekdayTurns = weekdayTurnInput.split(",");
 
         for (String weekdayTurn : weekdayTurns) {
             String trimmedTurn = weekdayTurn.trim();
             if (!trimmedTurn.isEmpty() && validateNickName(trimmedTurn)) {
-                weekdayTurnList.add(trimmedTurn);
+                if (uniqueNicknames.contains(trimmedTurn)) {
+                    ErrorMessages.nicknameDuplicateError();
+                    throw new IllegalArgumentException();
+                } else {
+                    weekdayTurnList.add(trimmedTurn);
+                    uniqueNicknames.add(trimmedTurn);
+                }
             } else {
                 ErrorMessages.nicknameLengthError();
                 throw new IllegalArgumentException();
--- src/main/java/oncall/View/ErrorMessages.java
+++ src/main/java/oncall/View/ErrorMessages.java
@@ -3,6 +3,7 @@ package oncall.View;
 public class ErrorMessages {
     private static final String DATE_NUMBER_TYPE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private static final String NICKNAME_LENGTH_ERROR_MESSAGE = "[ERROR] 닉네임은 5글자 이하입니다.";
+    private static final String NICKNAME_DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 사원을 입력하였습니다.";
 
     public static void dateNumberTypeError() {
         System.out.println(DATE_NUMBER_TYPE_ERROR_MESSAGE);
@@ -10,5 +11,8 @@ public class ErrorMessages {
 
     public static void nicknameLengthError() {
         System.out.println(NICKNAME_LENGTH_ERROR_MESSAGE);
+    }
+    public static void nicknameDuplicateError() {
+        System.out.println(NICKNAME_DUPLICATE_ERROR_MESSAGE);
     }
 }

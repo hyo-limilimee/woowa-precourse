fffab196fee0517925a5b202e6a78bed8175cd6c
hyo-limilimee <1001lily@naver.com>
2023-12-16 17:53:09 +0900

    fix: assignment error fixed

--- src/main/java/oncall/Model/MonthOfDay.java
+++ src/main/java/oncall/Model/MonthOfDay.java
@@ -12,34 +12,37 @@ public class MonthOfDay {
     public MonthOfDay(StartingDay startingDay) {
         this.startingDay = startingDay;
         this.weekendList = new ArrayList<>();
-        addWeekendsToList();
+//        addWeekendsToList();
     }
+//
+//    private void addWeekendsToList() {
+//        String[] daysOfWeek = {"월", "화", "수", "목", "금", "토", "일"};
+//
+//        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//
+//        int daysInCurrentMonth = daysInMonth[startingDay.getStartMonth()];
+//
+//        for (int i = 1; i <= daysInCurrentMonth; i++) {
+//            String dateKey = startingDay.getStartMonth() + "월 " + i + "일";
+//            String weekday = daysOfWeek[(i - 1) % 7];
+//
+//            boolean isWeekend = (startingDay.getStartMonth() == 1 && i == 1) ||
+//                    (startingDay.getStartMonth() == 3 && i == 1) ||
+//                    (startingDay.getStartMonth() == 5 && i == 5) ||
+//                    (startingDay.getStartMonth() == 6 && i == 6) ||
+//                    (startingDay.getStartMonth() == 8 && i == 15) ||
+//                    (startingDay.getStartMonth() == 10 && i == 3) ||
+//                    (startingDay.getStartMonth() == 10 && i == 9) ||
+//                    (startingDay.getStartMonth() == 12 && i == 25) ||
+//                    weekday.equals("토") || weekday.equals("일");
+//
+//            if (isWeekend) {
+//                weekendList.add(dateKey);
+//                System.out.println(dateKey);
+//            }
+//        }
+//    }
 
-    private void addWeekendsToList() {
-        String[] daysOfWeek = {"월", "화", "수", "목", "금", "토", "일"};
-
-        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        int daysInCurrentMonth = daysInMonth[startingDay.getStartMonth()];
-
-        for (int i = 1; i <= daysInCurrentMonth; i++) {
-            String dateKey = startingDay.getStartMonth() + "월 " + i + "일";
-            String weekday = daysOfWeek[(i - 1) % 7];
-
-            if ((startingDay.getStartMonth() == 1 && i == 1) ||
-                    (startingDay.getStartMonth() == 3 && i == 1) ||
-                    (startingDay.getStartMonth() == 5 && i == 5) ||
-                    (startingDay.getStartMonth() == 6 && i == 6) ||
-                    (startingDay.getStartMonth() == 8 && i == 15) ||
-                    (startingDay.getStartMonth() == 10 && i == 3) ||
-                    (startingDay.getStartMonth() == 10 && i == 9) ||
-                    (startingDay.getStartMonth() == 12 && i == 25) ||
-                    weekday.equals("토") || weekday.equals("일")) {
-                weekendList.add(dateKey);
-            }
-
-        }
-    }
 
     public Map<Integer, String> calculateWeekdays() {
         Map<Integer, String> weekdaysMap = new HashMap<>();
@@ -75,8 +78,8 @@ public class MonthOfDay {
             String dayOfWeek = weekdaysMap.get(i);
 
             String assignedPerson;
-            if (weekendList.contains(dateKey)) {
-                // 주말 목록에 있는 경우, holidayTurnList에서 배정
+            if (dayOfWeek.equals("토") || dayOfWeek.equals("일")) {
+                // 주말인 경우, holidayTurnList에서 직접 배정
                 int holidayIndex = holidayAssignedIndex % holidayTurnList.size();
                 assignedPerson = holidayTurnList.get(holidayIndex);
                 holidayAssignedIndex++; // 다음 배정된 사람을 위해 증가
@@ -94,7 +97,7 @@ public class MonthOfDay {
                     System.out.printf("%s %s %s%n", dateKey, dayOfWeek, assignedPerson);
                 }
             } else {
-                // 주말 목록에 없는 경우, weekdayTurnList에서 배정
+                // 주말이 아닌 경우, weekdayTurnList에서 배정
                 int weekdayIndex = weekdayAssignedIndex % weekdayTurnList.size();
                 assignedPerson = weekdayTurnList.get(weekdayIndex);
                 weekdayAssignedIndex++; // 다음 배정된 사람을 위해 증가
@@ -102,8 +105,6 @@ public class MonthOfDay {
             }
         }
     }
-
-
 
     private boolean isWeekend(String dayOfWeek) {
         return dayOfWeek.equals("토") || dayOfWeek.equals("일");
